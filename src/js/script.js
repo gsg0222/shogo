ccb11c40a3995ad1ffbcc5a7d13bf2b454dfa244
TSUCHIYA Shunsuke <gsg0222.1977@gmail.com>
2020-04-07 16:12:41 +0900

    Add img

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,3 @@
-alert("Hello");
-function changeBGC() {
-	let obj = document.getElementsByTagName("body");
-	for (let i = 0; i < obj.length; i++) {
-		obj[i].classList.toggle("bgc--mod");
-	}
-}
+$(document).ready(function () {
+	$('.drawer').drawer();
+});
