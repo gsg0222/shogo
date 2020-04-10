cee8629c23c2a7324e4fb1f07db4fe9e38f1a36d
TSUCHIYA Shunsuke <gsg0222.1977@gmail.com>
2020-04-10 17:58:53 +0900

    End faq

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,3 +2,14 @@ $(document).ready(function () {
 	$('.drawer').drawer();
 });
 AOS.init();
+// アコーディオン制御
+$('.faq__title').click(function () {
+	var $answer = $(this).next();
+	if ($answer.hasClass('faq__body--active')) {
+		$answer.removeClass('faq__body--active');
+		$answer.slideUp();
+	} else {
+		$answer.addClass('faq__body--active');
+		$answer.slideDown();
+	}
+});
