fc9441a4e5ff18494d812a75b0eecb31b704eb83
TSUCHIYA Shunsuke <gsg0222.1977@gmail.com>
2020-04-12 18:05:49 +0900

    End coding

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,61 @@ $('.faq__title').click(function () {
 		$answer.slideDown();
 	}
 });
+
+// Swiper初期化
+var swiper = new Swiper('.swiper-container', {
+	loop: true,
+	slidesPerView: 1.8,
+	spaceBetween: 10,
+	centeredSlides: true,
+	initialSlide: 1,
+	breakpoints: {
+		768: {
+			spaceBetween: 30,
+			slidesPerView: 2.5
+		},
+		992: {
+			spaceBetween: 56,
+			slidesPerView: 3.6
+		}
+	},
+});
+
+// ajaxでのフォーム投稿
+$(document).ready(function () {
+
+	$('#form').submit(function (event) {
+		var formData = $('#form').serialize();
+		$.ajax({
+			url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScipCJIkTvUuuhg1O5cFHIerpkiqmZ5PfJwZxqQFqwBuler_Q/formResponse",
+			data: formData,
+			type: "POST",
+			dataType: "xml",
+			statusCode: {
+				0: function () {
+					$(".contact__success").slideDown();
+					$(".contact__btn").fadeOut();
+					//window.location.href = "thanks.html";
+				},
+				200: function () {
+					$(".contact__false").slideDown();
+				}
+			}
+		});
+		event.preventDefault();
+	});
+
+});
+
+// スムーススクロール
+$(function () {
+	$('a[href^="#"]').on("click", function () {
+		var speed = 600;
+		var header_height = $("header").height();
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top - header_height;
+		$('body,html').animate({ scrollTop: position }, speed, 'swing');
+		return false;
+	});
+});
