15bcc20e90106d3b32f4e8d3f707839246516d59
TSUCHIYA Shunsuke <gsg0222.1977@gmail.com>
2020-04-13 14:58:40 +0900

    Feedback commited

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,7 @@ AOS.init();
 // アコーディオン制御
 $('.faq__title').click(function () {
 	var $answer = $(this).next();
-	if ($answer.hasClass('faq__body--active')) {
-		$answer.removeClass('faq__body--active');
-		$answer.slideUp();
-	} else {
-		$answer.addClass('faq__body--active');
-		$answer.slideDown();
-	}
+	$answer.slideToggle();
 });
 
 // Swiper初期化
